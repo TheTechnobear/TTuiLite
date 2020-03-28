cf39c6f4297d6afaf85892c68d98547cb36e5e83
Mark Harris <mharris@technobear.com>
2020-03-28 14:33:34 +0100

    compile, but NOT work on mac etc

--- docs/BUILD.md
+++ docs/BUILD.md
@@ -2,53 +2,15 @@
 git submodule update --init --recursive
 
 # Dependancies
-CMake
+CMake, i2cdev
 
-# Building linux (or mac)
+
+# Building linux (
 
     sudo apt install git
     sudo apt install cmake
+    sudo apt-get install -y i2c-tools
 
     mkdir build
     cd build
     cmake .. 
-
-
-# Building Mac (with xcode)
-
-    brew install git
-    brew install cmake
- 
-
-    mkdir build
-    cd build
-    cmake .. -GXcode 
-
-    xcodebuild -project ttuilite.xcodeproj -target ALL_BUILD -configuration MinSizeRel
-
-or
-
-    cmake --build .
-
-
-# Building within Sublime Text
-
-you can also build within sublime
-
-    mkdir build
-    cd build
-    cmake -G "Sublime Text 2 - Unix Makefiles" .. 
-
-
-# Building on Bela
-the bela web ui does not support a subdirectories, so the way install is
-  
-    mkdir -p ~/projects
-    cd ~/projects 
-    // git clone ttuilite.git here
-    mkdir build
-    cd build
-    cmake .. 
-
-
-
---
+++ ttuilite/TTuiDeviceNop.cpp
@@ -0,0 +1,64 @@
+#include "TTuiDevice.h"
+
+#include <iostream>
+
+namespace TTuiLite {
+
+
+TTuiDevice::TTuiDevice() {
+    throw std::runtime_error("Unsupported Platform");
+}
+
+TTuiDevice::~TTuiDevice() {
+}
+
+void TTuiDevice::start() {
+}
+
+void TTuiDevice::stop() {
+}
+
+unsigned TTuiDevice::process(bool) {
+    return 0;
+}
+
+void TTuiDevice::addCallback(std::shared_ptr<TTuiCallback> ) {
+}
+
+bool TTuiDevice::buttonState(unsigned ) {
+    return false;
+}
+
+unsigned TTuiDevice::numEncoders() {
+    return 0;
+}
+
+unsigned TTuiDevice::numButtons() {
+    return 0;
+}
+
+void TTuiDevice::displayPaint() {
+}
+
+void TTuiDevice::displayClear(unsigned ) {
+}
+
+void TTuiDevice::displayText(unsigned , unsigned , unsigned , unsigned , const std::string &) {
+}
+
+
+void TTuiDevice::clearText(unsigned , unsigned , unsigned ) {
+}
+
+void TTuiDevice::drawBitmap(unsigned , unsigned , unsigned , const char *) {
+}
+
+void TTuiDevice::clearRect(unsigned , unsigned , unsigned , unsigned , unsigned w, unsigned ) {
+}
+
+void TTuiDevice::drawText(unsigned , unsigned , unsigned , unsigned , const std::string &) {
+}
+
+
+}// namespace
+
